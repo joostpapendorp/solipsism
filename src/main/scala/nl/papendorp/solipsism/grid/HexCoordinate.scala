5a68367749cabd0539e881be0400ec5e6a35dfb1
Papendorp <joost.papendorp@gmail.com>
2018-02-11 16:43:28 +0100

    Directions literals for each of the cardinal increments. While in theory these are implicit, it bears spelling out to facilitate usage

--- src/main/scala/nl/papendorp/solipsism/grid/HexCoordinate.scala
+++ src/main/scala/nl/papendorp/solipsism/grid/HexCoordinate.scala
@@ -71,3 +71,37 @@ sealed trait Rotation
 
 	def rotate60( coordinate: HexCoordinate ): HexCoordinate
 }
+
+
+/*
+ * a Direction represents a single step along an axis, keeping that position constant.
+ * "Up" and "Down" are a bit arbitrary, because each step increases the first of the other two axes, while decrementing
+ * the second. Each "Up" inverts its "Down" counterpart.
+ * Taken all together, they form a circle around Origin.
+ * For example: XUp walks along the x-axis, thus keeping x constant. It increments y and decrements z. It inverts XDown.
+ */
+object Direction
+{
+
+	// in order of clockwise rotation:
+	case object XUp extends Direction //   (  0, +y, -z )
+
+	case object YUp extends Direction //   ( +x,  0, -z )
+
+	case object ZUp extends Direction //   ( +x, -y,  0 )
+
+	case object XDown extends Direction // (  0, -y, +z )
+
+	case object YDown extends Direction // ( -x,  0, +z )
+
+	case object ZDown extends Direction // ( -x, +y,  0 )
+
+	lazy val directions: Map[ Direction, HexCoordinate ] = {
+		val names = Seq( XUp, YUp, ZUp, XDown, YDown, ZDown )
+		names zip HexCoordinate( 0, 1 ).circle toMap
+	}
+}
+
+sealed trait Direction
+{
+}
--- src/test/scala/nl/papendorp/solipsism/grid/HexCoordinateTest.scala
+++ src/test/scala/nl/papendorp/solipsism/grid/HexCoordinateTest.scala
@@ -1,6 +1,7 @@
 package nl.papendorp.solipsism.grid
 
 
+import nl.papendorp.solipsism.grid.Direction._
 import nl.papendorp.solipsism.grid.HexCoordinate.Origin
 import nl.papendorp.solipsism.grid.Rotation.{Clockwise, CounterClockwise}
 import org.scalacheck.Arbitrary._
@@ -201,6 +202,43 @@ class HexCoordinateTest
 			}
 
 			check( forAll( rotations )( mirrorsOppositeDirection ) )
+		}
+	}
+
+	"All directions" should {
+		val allDirections = Seq( XUp, YUp, ZUp, XDown, YDown, ZDown )
+
+		"move along their axes" in {
+			allDirections foreach {
+				case direction@(XUp | XDown) => directions( direction ).x should be( 0 )
+				case direction@(YUp | YDown) => directions( direction ).y should be( 0 )
+				case direction@(ZUp | ZDown) => directions( direction ).z should be( 0 )
+				case _ => fail
+			}
+		}
+
+		"invert to their counterparts" in {
+			-directions( XUp ) should be( directions( XDown ) )
+			-directions( YUp ) should be( directions( YDown ) )
+			-directions( ZUp ) should be( directions( ZDown ) )
+		}
+
+		"form a circle together" in {
+			val circle = HexCoordinate( 0, 1 ).circle
+			val actualDirections = allDirections map directions
+
+			circle zip actualDirections foreach {
+				case (expected, actual) => expected should be( actual )
+				case _ => fail
+			}
+		}
+
+		"have 6 distinct values" in {
+			allDirections.toSet.size should be( 6 )
+		}
+
+		"have size 1" in {
+			allDirections foreach (directions( _ ).size should be( 1 ))
 		}
 	}
 }
